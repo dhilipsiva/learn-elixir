65fa59c67524d4daf5108eb76bce5fd72501b264
dhilipsiva <dhilipsiva@pm.me>
2020-08-23 08:58:00 +0530

    Basic opearators

---
+++ elixir/basic-operators.ex
@@ -0,0 +1,18 @@
+# String Concatenation
+
+IO.puts "String concatenation <>: #{ "Hello" <> " World!" }"
+
+
+# Boolean opearators
+
+IO.puts "or: #{true or "Some Text"}"
+IO.puts "or: #{false or "Some Text"}"
+IO.puts "false: #{false and raise "This error will never be raised"}"
+IO.puts "!: #{!false}"
+IO.puts "!: #{!"Something else"}"
+
+
+# Comparision Operators
+
+IO.puts "==, !=, ===, !==, <=, >=, < and >"
+IO.puts "#{1 == 1} #{1 != 2} #{1 < 2} #{1 == 1.0} #{1 === 1.0}"
